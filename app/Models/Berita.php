0370f2cb9e8611377c07b292b8affdd06c6fefac
Osas997 <larasatyt83@gmail.com>
2025-07-30 00:15:26 +0700

    feat: berita management

--- app/Models/Berita.php
+++ app/Models/Berita.php
@@ -9,4 +9,20 @@ class Berita extends Model
 {
     /** @use HasFactory<\Database\Factories\BeritaFactory> */
     use HasFactory;
+
+    protected $table = 'beritas';
+
+    protected $fillable = [
+        'slug',
+        'judul',
+        'isi',
+        'sumber',
+        'gambar',
+        'status',
+    ];
+
+    public function scopePublished($query)
+    {
+        return $query->where('status', 'published');
+    }
 }
