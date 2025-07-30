4a93695d12933025c70ddf13c39d47f02adeaf9f
Osas997 <larasatyt83@gmail.com>
2025-07-30 20:05:34 +0700

    feat: konotatif

---
+++ app/Enums/KonotatifKategori.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum KonotatifKategori: string
+{
+    case NOMINA = 'nomina';
+    case VERBA = 'verba';
+    case ADJEKTIVA = 'adjektiva';
+    case ADVERBIA = 'adverbia';
+}
