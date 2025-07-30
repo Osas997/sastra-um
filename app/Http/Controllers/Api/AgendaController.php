aac5ed38c83b776b8194cad9e36dd5dd806ff551
Hidayat <ahmadhidayatpermana202@gmail.com>
2025-07-30 10:34:11 +0700

    Fix Agenda

--- app/Http/Controllers/Api/AgendaController.php
+++ app/Http/Controllers/Api/AgendaController.php
@@ -1,50 +1,68 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\Agenda;
 use App\Http\Requests\StoreAgendaRequest;
 use App\Http\Requests\UpdateAgendaRequest;
+use Illuminate\Http\Request;
 
 class AgendaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $query = Agenda::query();
+
+        if ($request->has('search')) {
+            $query->where('judul', 'like', '%' . $request->search . '%');
+        }
+
+        return response()->json($query->latest()->get());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    public function show($id)
+    {
+        $agenda = Agenda::find($id);
+
+        if (!$agenda) {
+            return response()->json(['message' => 'Agenda tidak ditemukan'], 404);
+        }
+
+        return response()->json($agenda);
+    }
+
     public function store(StoreAgendaRequest $request)
     {
-        //
+        $data = $request->validated();
+        $agenda = Agenda::create($data);
+
+        return response()->json($agenda, 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Agenda $agenda)
+    public function update(UpdateAgendaRequest $request, $id)
     {
-        //
+        $agenda = Agenda::find($id);
+
+        if (!$agenda) {
+            return response()->json(['message' => 'Agenda tidak ditemukan'], 404);
+        }
+
+        $agenda->update($request->validated());
+
+        return response()->json($agenda);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateAgendaRequest $request, Agenda $agenda)
+    public function destroy($id)
     {
-        //
-    }
+        $agenda = Agenda::find($id);
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Agenda $agenda)
-    {
-        //
+        if (!$agenda) {
+            return response()->json(['message' => 'Agenda tidak ditemukan'], 404);
+        }
+
+        $agenda->delete();
+
+        return response()->json(['message' => 'Agenda berhasil dihapus']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\FaqController;
 use App\Http\Controllers\API\SlideshowController;
+use App\Http\Controllers\Api\BeritaController;
+use App\Http\Controllers\Api\AgendaController;
 
+// login route
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
-Route::apiResource('faq', FaqController::class);
+//admin slideshow   
 Route::apiResource('slideshow', SlideshowController::class)->except(['show','update']);
 Route::post('slideshow/update', [SlideshowController::class, 'update']);
+
+//admin faq
+Route::apiResource('faq', FaqController::class);
+
+//admin agenda
+Route::apiResource('agenda', AgendaController::class);
